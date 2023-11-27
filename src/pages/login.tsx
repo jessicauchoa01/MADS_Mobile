29c52ccf6e37f91b797a239b0ff4e4f136ada92a
TADEK <tylersousa9@hotmail.com>
2023-11-27 10:39:09 -0100

    gg

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
+import React, { useState } from "react";
 import {
-  IonBackButton,
+  IonAlert,
   IonButton,
-  IonButtons,
   IonCol,
   IonContent,
   IonGrid,
@@ -10,27 +10,71 @@ import {
   IonPage,
   IonRow,
 } from "@ionic/react";
-import { Link } from "react-router-dom";
-import React from "react";
-import { arrowBackOutline } from "../../node_modules/ionicons/icons";
+import { arrowBackOutline } from "ionicons/icons";
 import personAddOutline from "../assets/Vector.svg";
 import goeat from "../assets/logo.svg";
 import logInOutline from "../assets/login.svg";
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 const Login: React.FC = () => {
-  const doLogin = (event: any) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // const alert = () => {
+  //   <IonAlert
+  //   header="sadas"
+  //   trigger="login"
+  //   buttons={[
+  //     {
+  //       text: 'OK',
+  //       role: 'confirm',
+  //       handler: () => {
+  //         console.log();
+  //       },
+  //     },
+  //   ]}
+  //   onDidDismiss={({ detail }) => console.log(`Dismissed with role: ${detail.role}`)}
+  // ></IonAlert>
+  // }
+
+  const doLogin = async (event: React.FormEvent) => {
     event.preventDefault();
-    console.log("doLogin");
+
+    try {
+      const response = await fetch(
+        "http://localhost/MADS_Web/sourceWeb/sourceMobile/loginValidaMobile.php",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email: username, password: password }),
+        }
+      );
+
+      const responseData = await response.json();
+
+      if (response.ok) {
+        // Login bem-sucedido, redirecione ou faça qualquer outra ação necessária
+        console.log(responseData);
+        window.location.href = "/homepage";
+      } else {
+        // Exiba uma mensagem de erro ou realize ações específicas em caso de falha
+        console.error(responseData);
+        //window.location.href = "/login";
+      }
+    } catch (error) {
+      console.error("Erro na solicitação:", error);
+    }
   };
 
   return (
     <IonPage className="loginPage">
       <IonContent scrollY={false}>
         <div className="ion-text-center ion-padding">
-          <img src={goeat} width={"150px"} />
+          <img src={goeat} width={"150px"} alt="Logo" />
         </div>
-        <IonButtons slot="start" className="arrow-back"></IonButtons>
         <div className="main">
           <div className="seta">
             <Link to="/homepage">
@@ -48,55 +92,53 @@ const Login: React.FC = () => {
                       placeholder="Introduza o seu nome de utilizador"
                       label="Utilizador"
                       type="text"
-                    ></IonInput>
+                      value={username}
+                      onIonChange={(e) => setUsername(e.detail.value!)}
+                    />
                     <IonInput
                       className="input"
                       labelPlacement="floating"
                       placeholder="Introduza a sua password"
                       label="Password"
                       type="password"
-                    ></IonInput>
+                      value={password}
+                      onIonChange={(e) => setPassword(e.detail.value!)}
+                    />
                   </div>
                 </IonCol>
               </IonRow>
             </IonGrid>
+            <IonButton
+              className="login"
+              id="login"
+              size="large"
+              shape="round"
+              type="submit"
+              expand="block"
+            >
+              Login
+              <IonIcon icon={logInOutline} slot="end" />
+            </IonButton>
+            <IonAlert
+              header="é preciso ir buscar a mensagem!"
+              trigger="login"
+              onDidDismiss={({ detail }) =>
+                console.log(`Dismissed with role: ${detail.role}`)
+              }
+            ></IonAlert>
           </form>
-          <form onSubmit={doLogin}>
-            <IonGrid>
-              <IonRow>
-                <IonCol>
-                  <div className="button-login">
-                    <IonButton
-                      className="login"
-                      id="login"
-                      size="large"
-                      shape="round"
-                      routerLink="/homepage"
-                      type="submit"
-                      expand="block"
-                    >
-                      Login
-                      <IonIcon icon={logInOutline} slot="end" />
-                    </IonButton>
-                  </div>
-
-                  <IonButton
-                    className="registo"
-                    id="registar"
-                    size="large"
-                    fill="outline"
-                    shape="round"
-                    routerLink="/register"
-                    type="button"
-                    expand="block"
-                  >
-                    Registar
-                    <IonIcon icon={personAddOutline} slot="end" />
-                  </IonButton>
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-          </form>
+          <IonButton
+            className="registo"
+            id="registar"
+            size="large"
+            fill="outline"
+            shape="round"
+            routerLink="/register"
+            expand="block"
+          >
+            Registar
+            <IonIcon icon={personAddOutline} slot="end" />
+          </IonButton>
         </div>
       </IonContent>
     </IonPage>
