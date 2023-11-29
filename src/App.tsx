bb3fe159d7118e4ea31fe3a1cf6cf18b1b53b391
Mosca <Frofer245@gmail.com>
2023-11-29 16:35:34 -0100

    lógica correta

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ import "@ionic/react/css/display.css";
 import Login from "./pages/login";
 import Register from "./pages/Register";
 import Homepage from "./pages/Homepage";
-import Carrinho from "./pages/Carrinho";
+
 
 setupIonicReact();
 
@@ -36,7 +36,6 @@ const App: React.FC = () => (
         <Route component={Login} path="/login" />
         <Route component={Register} path="/register" />
         <Route component={Homepage} path="/homepage" />
-        <Route component={Carrinho} path="/carrinho" />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
