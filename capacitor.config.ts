a2e9f9e8e3045f68e823f8c694893f940533c203
André <andrerui-rodrigues@hotmail.com>
2023-11-29 10:10:48 -0100

    Página profile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'goeat',
+  appName: 'myApp',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import { Redirect, Route } from "react-router-dom";
-import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
+import { Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import Profile from './pages/Profile';
 
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import Login from "./pages/login";
-import Register from "./pages/Register";
-import Homepage from "./pages/Homepage";
-import Carrinho from "./pages/Carrinho";
+import './theme/variables.css';
 
 setupIonicReact();
 
@@ -31,12 +29,8 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonRouterOutlet>
         <Route exact path="/">
-          <Homepage />
+          <Profile />
         </Route>
-        <Route component={Login} path="/login" />
-        <Route component={Register} path="/register" />
-        <Route component={Homepage} path="/homepage" />
-        <Route component={Carrinho} path="/carrinho" />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
